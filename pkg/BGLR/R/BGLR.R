e048815ed7ba702441373d9d2eaef854308607f6
perpdgo <perpdgo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-08 01:25:09 +0000

    Update documentation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bglr@69 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BGLR/R/BGLR.R
+++ pkg/BGLR/R/BGLR.R
@@ -633,7 +633,7 @@ welcome=function()
   cat("\n");
   cat("#--------------------------------------------------------------------#\n");
   cat("#        _\\\\|//_                                                     #\n");
-  cat("#       (` o-o ')      BGLR v1.0 build 68                            #\n");
+  cat("#       (` o-o ')      BGLR v1.0 build 69                            #\n");
   cat("#------ooO-(_)-Ooo---------------------------------------------------#\n");
   cat("#                      Bayesian Generalized Linear Regression        #\n");
   cat("#                      Gustavo de los Campos, gdeloscampos@gmail.com #\n");
