85c4a91a993a876ede5433cafbfd62b73a74388e
perpdgo <perpdgo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-14 02:39:37 +0000

    adding methods.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bglr@79 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/BGLR/R/methods.R
@@ -0,0 +1,20 @@
+# file BGLR/methods.R
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 or 3 of the License
+#  (at your option).
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+
+#BGLR: A Statistical Package for Whole-Genome Regression & Prediction
+#Authors: Gustavo de los Campos & Paulino Perez Rodriguez
+#Birmingaham, Alabama, 2013, 2014
+
+
