1528cd0d5043b2457fae0b4fb7990866047586b7
perpdgo <perpdgo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-24 14:51:42 +0000

    Update methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bglr@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BGLR/R/methods.R
+++ pkg/BGLR/R/methods.R
@@ -24,21 +24,77 @@ print.BGLR=function(x,...)
 
 summary.BGLR=function(object,...)
 {
-	object
+	tmp<-paste('--------------------> Summary of data & model <--------------------')
+   	cat(tmp,'\n\n')
+
+   	tmp<-paste(' Number of phenotypes=', sum(!is.na(object$y)))
+        cat(tmp,'\n')
+
+	cat(' Min (TRN)= ', min(object$y,na.rm=TRUE),'\n')
+	cat(' Max (TRN)= ', max(object$y,na.rm=TRUE),'\n')
+	cat(' Variance of phenotypes (TRN)=', round(var(object$y,na.rm=TRUE),4),'\n') 
+ 	cat(' Variance of weighted phenotypes (TRN)=',round(var(object$y*object$weights,na.rm=TRUE),4),'\n')
+        cat(' Residual variance=',round(object$varE,4),'\n')
+
+        n<-length(object$y)
+
+        if(any(is.na(object$y)))
+	{     
+     		tst<-which(is.na(object$y))
+
+     		cat(' N-TRN=',n-length(tst), ' N-TST=',length(tst),'\n')
+     
+     		cat(' Correlation TRN:\n')
+
+     		cat('   - Standard= ',round(cor(object$y[-tst],object$yHat[-tst]),4),'\n')
+  
+     		cat('   - Weighted= ',round(cor((object$weights*object$y)[-tst],(object$weights*object$yHat)[-tst]),4),'\n')
+   }else{
+     cat(' N-TRN=',n,'  N-TST=0', '\n\n')
+
+     cat(' Correlation TRN:\n')
+     
+     cat('   - Standard= ',round(cor(object$y,object$yHat),4),'\n')
+
+     cat('   - Weighted= ',round(cor(object$weights*object$y,object$weights*object$yHat),4),'\n')
+
+   }
+  
+   cat('\n')
+   cat(' -- Information about the linear term -- \n')
+   cat('\n')
+   cat(' Model includes intercept by default\n')
+   
+   for(k in 1:length(object$ETA))
+   {
+        if(object$ETA[[k]]$model=="FIXED")
+	{
+		cat(" Coefficientes in ETA[",k,"] are asigned a flat prior\n")	
+	}else{
+		if(object$ETA[[k]]$model=="RKHS")
+		{
+			cat(" ETA[",k,"] assumed as a random effect normally distributed with \n covariance (or its eigendecoposition) provided by user \n");
+		}else{
+			cat(" Coefficientes in ETA[",k,"] modeled as in ", object$ETA[[k]]$model,"\n")
+		}
+	}
+   } 
+
+   cat('\n------------------------------------------------------------------\n');	
 }
 
 residuals.BGLR=function(object,...)
 {
-
+	object$y-object$yHat
 }
 
 predict.BGLR=function(object,newdata,...)
 {
-
+	object$yHat
 }
 
 effects.BGLR=function(object,...)
 {
-
+	object$ETA	
 }
 
